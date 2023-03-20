1bc22156dfff182035a56a6497917152d46ed05d
alejandrogh7 <alejo1garciasosa@gmail.com>
2023-03-20 13:50:24 -0500

    Header ready without logic

--- app/src/components/Header/Header.tsx
+++ app/src/components/Header/Header.tsx
@@ -2,13 +2,15 @@ import Score from "./Score";
 
 const Header = () => {
   return (
-    <header className="grid grid-cols-2 w-4/6 h-36 rounded-xl py-3 px-6 outline outline-offset-2 outline-border">
-      <div className="flex flex-col">
-        <p className="text-3xl font-extrabold uppercase text-white">rock</p>
-        <p className="text-3xl font-extrabold uppercase text-white">paper</p>
-        <p className="text-3xl font-extrabold uppercase text-white">scissors</p>
+    <header className="grid grid-cols-2 w-3/6 h-36 rounded-xl py-3 px-6 outline outline-offset-2 outline-border">
+      <div className="flex flex-col tracking-wide">
+        <p className="text-3xl font-semibold uppercase text-white">
+          rock
+        </p>
+        <p className="text-3xl font-semibold uppercase text-white">paper</p>
+        <p className="text-3xl font-semibold uppercase text-white">scissors</p>
       </div>
-      <div className="">
+      <div className="relative">
         <Score />
       </div>
     </header>
