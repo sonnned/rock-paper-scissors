4bf71218be588a12eae7313811af6af17cc320fd
alejandrogh7 <alejo1garciasosa@gmail.com>
2023-03-20 13:51:23 -0500

    Adding new components to the root page

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Rules from "./components/Rules/Rules";
 
 function App() {
   return (
     <div className="w-screen h-screen bg-main font-barlow">
-      <main className="h-screen w-screen py-8 px-10 flex flex-col items-center justify-start">
+      <main className="h-screen w-screen py-8 px-10 flex flex-col items-center justify-start relative">
         <Header />
+        <Rules />
       </main>
     </div>
   );
