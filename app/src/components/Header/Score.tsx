3c04273e59292672398384761bc880017a15d32c
alejandrogh7 <alejo1garciasosa@gmail.com>
2023-03-20 13:50:13 -0500

    Score view in the header component

--- app/src/components/Header/Score.tsx
+++ app/src/components/Header/Score.tsx
@@ -1,5 +1,14 @@
 const Score = () => {
-  return <div>ss</div>;
+  return (
+    <div className="flex flex-col align-center justify-center h-full w-40 border bg-white rounded-xl absolute right-0">
+      <p className="text-xl text-center uppercase font-semibold text-score">
+        Score
+      </p>
+      <p className="text-6xl text-center tracking-wide font-bold text-dark">
+        12
+      </p>
+    </div>
+  );
 };
 
 export default Score;
