c64d5d216836b101c0ff1fdc03639f3cc4b2296f
alejandrogh7 <alejo1garciasosa@gmail.com>
2023-03-21 12:26:27 -0500

    Adding the rules component and adding the button to see the rules and adding the logic to change the context

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,15 +1,23 @@
-import React from "react";
-import logo from "./logo.svg";
+import { useContext, useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Rules from "./components/Rules/Rules";
+import { AppContext } from "./context/AppContext";
 
 function App() {
+  const { rules, setRules } = useContext(AppContext);
+
   return (
     <div className="w-screen h-screen bg-main font-barlow">
       <main className="h-screen w-screen py-8 px-10 flex flex-col items-center justify-start relative">
         <Header />
-        <Rules />
+        {rules ? <Rules /> : null}
+        <p
+          className="uppercase border px-6 py-2 text-center rounded-lg absolute bottom-0 right-0 m-10 text-white font-semibold cursor-pointer z-0"
+          onClick={() => setRules(true)}
+        >
+          rules
+        </p>
       </main>
     </div>
   );
