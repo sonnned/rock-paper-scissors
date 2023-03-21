c70ce3b74befa40c6c01057b6e8e4ef6632e046b
alejandrogh7 <alejo1garciasosa@gmail.com>
2023-03-21 12:25:58 -0500

    Creting the rules lightbox with all the logic and the rules

--- app/src/components/Rules/Rules.tsx
+++ app/src/components/Rules/Rules.tsx
@@ -1,15 +1,25 @@
+import { useContext } from "react";
 import close from "../../assets/icon-close.svg";
+import rules from "../../assets/image-rules.svg";
+import { AppContext } from "../../context/AppContext";
 
 const Rules = () => {
+  const { setRules } = useContext(AppContext);
+
   return (
-    <div className="absolute top-0 right-0 left-0 bottom-0 bg-back">
-      <div className="relative top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white h-[55%] w-1/3 rounded-xl flex flex-col p-7">
-        <div className="flex flex-row w-full justify-between align-center">
+    <div className="absolute top-0 right-0 left-0 bottom-0 bg-back z-50">
+      <div className="relative top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white h-[60%] w-[25%] rounded-xl flex flex-col p-7">
+        <div className="flex flex-row w-full justify-between align-center pb-10">
           <p className="uppercase text-3xl font-bold text-dark">rules</p>
-          <img className="w-10 h-10 p-2" src={close} alt="close button" />
+          <img
+            className="w-10 h-10 p-2 cursor-pointer"
+            src={close}
+            alt="close button"
+            onClick={() => setRules(false)}
+          />
         </div>
-        <div>
-          
+        <div className="flex align-center justify-center">
+          <img src={rules} alt="rules image" />
         </div>
       </div>
     </div>
