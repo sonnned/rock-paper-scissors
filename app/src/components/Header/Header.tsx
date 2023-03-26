966760843550414d2bd05023af7248d516e6139e
alejandrogh7 <alejo1garciasosa@gmail.com>
2023-03-25 19:18:15 -0500

    Header refactor to change text to image logo

--- app/src/components/Header/Header.tsx
+++ app/src/components/Header/Header.tsx
@@ -1,14 +1,11 @@
 import Score from "./Score";
+import logo from "../../assets/logo.svg";
 
 const Header = () => {
   return (
     <header className="grid grid-cols-2 w-3/6 h-36 rounded-xl py-3 px-6 outline outline-offset-2 outline-border">
-      <div className="flex flex-col tracking-wide">
-        <p className="text-3xl font-semibold uppercase text-white">
-          rock
-        </p>
-        <p className="text-3xl font-semibold uppercase text-white">paper</p>
-        <p className="text-3xl font-semibold uppercase text-white">scissors</p>
+      <div className="flex">
+        <img src={logo} alt="logo" />
       </div>
       <div className="relative">
         <Score />
