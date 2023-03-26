33c738913e47253622ee47bf5fc85c46ac64043c
alejandrogh7 <alejo1garciasosa@gmail.com>
2023-03-25 20:34:58 -0500

    Adding logic to connect with the score

--- app/src/components/Body/Icon.tsx
+++ app/src/components/Body/Icon.tsx
@@ -1,3 +1,6 @@
+import { useContext } from "react";
+import { AppContext } from "../../context/AppContext";
+
 const Icon = ({
   image,
   positions,
@@ -8,11 +11,18 @@ const Icon = ({
   background: string;
 }) => {
   const positions_string = positions.join(" ");
+  const { setSelected } = useContext(AppContext);
+
   return (
     <div
       className={`content-container absolute ${positions_string} flex justify-center items-center`}
     >
-      <div className={`h-max w-max rounded-full bg-${background}`}>
+      <div
+        className={`h-max w-max rounded-full bg-${
+          background ? background : ""
+        } cursor-pointer`}
+        onClick={() => setSelected(background)}
+      >
         <div className="flex justify-center items-center h-36 w-36 bg-white shadow-lg rounded-full m-6 shadow-icon">
           <img src={image} alt={`${background} icon`} />
         </div>
