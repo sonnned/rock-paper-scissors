ca303753fa5931fa48f3d2e6e6b7128a28fe742c
alejandrogh7 <alejo1garciasosa@gmail.com>
2023-03-25 20:35:08 -0500

    Adding logi to the score

--- app/src/components/Header/Score.tsx
+++ app/src/components/Header/Score.tsx
@@ -1,11 +1,15 @@
+import { useContext } from "react";
+import { AppContext } from "../../context/AppContext";
+
 const Score = () => {
+  const { score } = useContext(AppContext);
   return (
     <div className="flex flex-col align-center justify-center h-full w-40 border bg-white rounded-xl absolute right-0">
       <p className="text-xl text-center uppercase font-semibold text-score">
         Score
       </p>
       <p className="text-6xl text-center tracking-wide font-bold text-dark">
-        12
+        {score}
       </p>
     </div>
   );
