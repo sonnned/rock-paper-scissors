d8a94d729e47065c737d50a7fa641b1124900253
alejandrogh7 <alejo1garciasosa@gmail.com>
2023-03-25 20:34:35 -0500

    Updating context the score context

--- app/src/context/AppContext.tsx
+++ app/src/context/AppContext.tsx
@@ -11,6 +11,8 @@ export interface AppContextInterface {
   setRules: Dispatch<SetStateAction<boolean>>;
   selected: string;
   setSelected: Dispatch<SetStateAction<string>>;
+  score: number;
+  setScore: Dispatch<SetStateAction<number>>;
 }
 
 const defaultStore = {
@@ -18,6 +20,8 @@ const defaultStore = {
   setRules: (e: boolean) => {},
   selected: "",
   setSelected: (e: string) => {},
+  score: 0,
+  setScore: (e: number) => {},
 };
 
 export const AppContext = createContext(defaultStore);
@@ -29,9 +33,12 @@ type AppProviderProps = {
 export default function AppProvider({ children }: AppProviderProps) {
   const [rules, setRules] = useState<boolean>(false);
   const [selected, setSelected] = useState<string>("");
+  const [score, setScore] = useState<number>(0);
 
   return (
-    <AppContext.Provider value={{ rules, setRules, selected, setSelected }}>
+    <AppContext.Provider
+      value={{ rules, setRules, selected, setSelected, score, setScore }}
+    >
       {children}
     </AppContext.Provider>
   );
