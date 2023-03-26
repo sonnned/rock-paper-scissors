aa74022526cf76c2088c8b234223cfc34172c974
alejandrogh7 <alejo1garciasosa@gmail.com>
2023-03-25 19:17:26 -0500

    Adding new colors and box shadow

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -9,9 +9,15 @@ module.exports = {
         dark: "hsl(229, 25%, 31%)",
         back: "rgba(1,1,1,0.3)",
       },
+      boxShadow: {
+        "icon": "inset 0px 10px 0px 0px rgba(0,0,0,0.1)",
+      },
     },
     backgroundImage: {
       main: "radial-gradient(hsl(214, 47%, 23%), hsl(237, 49%, 15%))",
+      rock: "radial-gradient(hsl(349, 71%, 52%), hsl(349, 70%, 56%))",
+      paper: "radial-gradient(hsl(230, 89%, 62%), hsl(230, 89%, 65%))",
+      scissors: "radial-gradient(hsl(39, 89%, 49%), hsl(40, 84%, 53%))",
     },
     fontFamily: {
       barlow: ["Barlow Semi Condensed", "sans-serif"],
