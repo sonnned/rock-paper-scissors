350844dbb9922dd1b137a3e5eee9091a5275512d
alejandrogh7 <alejo1garciasosa@gmail.com>
2023-03-25 19:18:25 -0500

    Adding body to app

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect } from "react";
 import "./App.css";
+import Body from "./components/Body/Body";
 import Header from "./components/Header/Header";
 import Rules from "./components/Rules/Rules";
 import { AppContext } from "./context/AppContext";
@@ -11,6 +12,7 @@ function App() {
     <div className="w-screen h-screen bg-main font-barlow">
       <main className="h-screen w-screen py-8 px-10 flex flex-col items-center justify-start relative">
         <Header />
+        <Body />
         {rules ? <Rules /> : null}
         <p
           className="uppercase border px-6 py-2 text-center rounded-lg absolute bottom-0 right-0 m-10 text-white font-semibold cursor-pointer z-0"
